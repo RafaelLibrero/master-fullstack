821fc2a40d32d919dd9aa63301653b34e19af78d
Rafael Librero <rafael.librero@radmas.com>
2022-11-16 16:48:57 +0100

    Video service y formulario nuevo video

--- videos-angular/src/app/components/video-new/video-new.component.ts
+++ videos-angular/src/app/components/video-new/video-new.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { Video } from 'src/app/models/video';
 
 @Component({
-  selector: 'app-video-new',
+  selector: 'video-new',
   templateUrl: './video-new.component.html',
-  styleUrls: ['./video-new.component.css']
+  styleUrls: ['./video-new.component.css'],
+  providers: [UserService]
 })
 export class VideoNewComponent implements OnInit {
 
   public page_title: string
+  public identity
+  public token
+  public video: Video
+  public status: string
 
-  constructor() {
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
+  ) {
     this.page_title = "Guardar un nuevo video favorito"
+    this.identity = this._userService.getIdentity()
+    this.token = this._userService.getToken()
+    this.video = new Video(1, this.identity.sub, '', '', '', '', null, null)
+    this.status = ""
   }
 
   ngOnInit(): void {
+    
+  }
+
+  onSubmit(form: any){
+    
   }
 
 }
---
+++ videos-angular/src/app/services/video.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Video } from '../models/video';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideoService {
+
+  public api_url: string
+
+  constructor(
+    public _http: HttpClient
+  ) {
+    this.api_url = environment.API_URL;
+  }
+
+  create(token: any, video: any):Observable<any>{
+    let json = JSON.stringify(video);
+    let params = 'json='+json;
+
+    let headers = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+
+    return this._http.post(this.api_url+'video/new', params, {headers:headers});
+  }
+
+  getVideos(token: any):Observable<any>{
+    let headers = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+
+    return this._http.get(this.api_url+'video', {headers:headers});
+  }
+
+  getVideo(token: any, id: any):Observable<any>{
+    let headers = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+
+    return this._http.get(this.api_url+'video/'+id, {headers:headers});
+  }
+
+  update(token: any, video: any, id: any):Observable<any>{
+    let json = JSON.stringify(video);
+    let params = 'json='+json;
+
+    let headers = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+
+    return this._http.put(this.api_url+'video/'+id, params, {headers:headers});
+  }
+
+  delete(token: any, id: any):Observable<any>{
+    let headers = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token);
+
+    return this._http.delete(this.api_url+'video/'+id, {headers:headers});
+  }
+  
+}
