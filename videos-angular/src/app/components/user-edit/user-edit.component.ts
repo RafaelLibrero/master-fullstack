341c7a9e6ce33cc959c38f246155f863a57b0724
Rafael Librero <rafael.librero@radmas.com>
2022-11-16 16:07:47 +0100

    Ajustes de usuario

--- videos-angular/src/app/components/user-edit/user-edit.component.ts
+++ videos-angular/src/app/components/user-edit/user-edit.component.ts
@@ -1,19 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
 
-  public page_title: string
+  public page_title: string 
+  public user: User
+  public status: string
+  public identity: any
+  public token: any
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = "Ajustes de usuario"
+    this.identity = _userService.getIdentity()
+    this.token = _userService.getToken()
+    this.user = new User(
+      this.identity.sub, this.identity.name,
+      this.identity.surname, this.identity.email, '', 'ROLE_USER', '')
+    this.status = ""
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: any){
+    this._userService.update(this.token, this.user).subscribe(
+      response => {
+        if(response && response.status == 'success'){
+          this.status = "success"
+
+          this.identity = response.user
+          this.user = response.user
+          localStorage.setItem('identity', JSON.stringify(this.identity))
+          
+        }else{
+          this.status = "error"
+        }
+      },
+      error => {
+        this.status = "error"
+        console.log(error)
+      }
+    )
   }
 
 }
